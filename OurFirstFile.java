693326fdb4dc3d771733d6ab69f3278d9df47ea9
MatDls <mdzielinski@gmail.com>
2018-02-17 13:29:48 +0100

    Autor rozdzialu zdefiniowany

--- OurFirstFile.java
+++ OurFirstFile.java
@@ -3,7 +3,7 @@ Tematem przewodnim gry będą Krucjaty.
 Rozdział 1 Janusza
 ------------------------------------
 First chapter of this notable book will be about friendship.
-
+This chapter will be written by Janush.
 
 
 
