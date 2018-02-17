9a97b807fa39b25626bfbe6da54b50584f3e169e
MatDls <mdzielinski@gmail.com>
2018-02-17 13:00:58 +0100

    Rodzdizaly Janusza Mariusza i Ziutka

---
+++ OurFirstFile.java
@@ -0,0 +1,24 @@
+Tematem przewodnim gry będą Krucjaty.
+
+Rozdział 1 Janusza
+------------------------------------
+
+
+
+
+
+Rozdział 2 Mariusza
+------------------------------------
+
+
+
+
+
+
+Rozdział 3 Ziutka
+------------------------------------
+
+
+
+
+
